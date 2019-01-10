8ed1ad02a5364a4a05fcc083fb9a6b146cbff6f6
Madhur <bhattad.madhur1995@gmail.com>
2019-01-10 16:44:58 -0500

    Add files via upload

--- src/fills/Fill.java
+++ src/fills/Fill.java
@@ -1,1 +1,54 @@
+package fills;
 
+import orderSpecs.ClientId;
+import orderSpecs.ClientOrderId;
+import orderSpecs.Quantity;
+//import orderSpecs.MarketId;
+//import orderTypes.RestingOrder;
+import orderTypes.SweepingOrder;
+
+public class Fill 
+{
+	private SweepingOrder sweepedOrder;
+	private ClientId CounterPartyId;
+	
+	public Fill()
+	{
+		//MarketId Mid = R.getMarketId();
+	}
+
+	public SweepingOrder getSweepedOrder() 
+	{
+		return sweepedOrder;
+	}
+
+	public void setSweepedOrder(SweepingOrder sweepedOrder) 
+	{
+		this.sweepedOrder = sweepedOrder;
+	}
+
+	public ClientId getClientId()
+	{
+		return this.getSweepedOrder().getClientId();
+	}
+	
+	public Quantity getQuantity()
+	{
+		return this.getSweepedOrder().getQuantity();
+	}
+	
+	public ClientOrderId getClientOrderId()
+	{
+		return this.getSweepedOrder().getClientOrderId();
+	}
+	
+	public ClientId getCounterpartyId() 
+	{
+		return CounterPartyId;
+	}
+
+	public void setCounterpartyId(ClientId counterPartyId) 
+	{
+		CounterPartyId = counterPartyId;
+	}
+}
